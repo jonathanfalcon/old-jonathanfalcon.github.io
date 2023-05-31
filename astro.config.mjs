29f7bd2359042bfd43f4d18729fa9deb2f8baa7d
Jonathan Falcon <JQFalcon@outlook.com>
2023-05-30 22:31:42 -0500

    Added Histogram.astro component to create histograms with chart.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -77,6 +77,7 @@ export default defineConfig({
       alias: {
         '~': path.resolve(__dirname, './src'),
         'postImages': path.resolve(__dirname, './src/assets/images/posts'),
+        'postJson': path.resolve(__dirname, './src/assets/json/posts')
       },
     },
   },
